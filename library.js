af7c582805f505043d589e4c853daec32c23b96b
mattmiller1989 <mattmiller0989@gmail.com>
2021-01-01 12:43:41 -0600

    added style

--- library.js
+++ library.js
@@ -1,3 +1,5 @@
 function openBookForm(){
-    console.log('Here is where you add a fucking book!!11');
+    //console.log('Here is where you add a fucking book!!11');
+    const bookForm=document.getElementById('newBookForm');
+    bookForm.style.display='block';
 }
