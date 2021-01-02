2decaeb24a41b65d4edd15fe100c6d8e4634ec10
mattmiller1989 <mattmiller0989@gmail.com>
2021-01-02 12:46:07 -0600

    Fixed submit button so form wont close without valid input

--- library.js
+++ library.js
@@ -1,22 +1,40 @@
+const bookForm=document.getElementById('newBookForm');
+//bookForm.classList.add('invisible');
 function openBookForm(){
-    //console.log('Here is where you add a fucking book!!11');
-    const bookForm=document.getElementById('newBookForm');
-    bookForm.style.display='block';
+
+    bookForm.classList.remove('invisible');
+    bookForm.classList.add('visible');
+
+    
 }
-function submitBook(){
+function checkInput(){
+
     let title=document.querySelector('#title').value;
     let author=document.querySelector('#author').value;
     let numPages=document.querySelector('#numPages').value;
     let hasRead=document.querySelector('#hasRead').checked;
 
-    console.log(`title: ${title}\nauthor: ${author}\nnumPages: ${numPages}\nhasRead: ${hasRead}\n`);
+    if(!(title && author && numPages)){
+        console.log('Title, author and page number cannot be left empty');
+     
+        
+    }else{
 
-    closeForm();
+        console.log(`Title: ${title}\nAuthor: ${author}\n# of pages: ${numPages}\nHas Read?: ${hasRead}`);
+       // submitBook(title,author,numPages,hasRead)
+    }
+   
+}
+function submitBook(title,author,numPages,hasRead){
+   
+    
+    
 }
 function cancelBook(){
     closeForm();
 }
 function closeForm(){
-    const bookForm=document.getElementById('newBookForm');
-    bookForm.style.display='none';
+   // const bookForm=document.getElementById('newBookForm');
+    //bookForm.style.display='none';
+
 }
