5eb7611320ed3f9f083960f0e29d6d7037bbbaf1
mattmiller1989 <mattmiller0989@gmail.com>
2021-01-02 12:55:18 -0600

    Fixed DOM to close form properly

--- library.js
+++ library.js
@@ -21,20 +21,25 @@ function checkInput(){
     }else{
 
         console.log(`Title: ${title}\nAuthor: ${author}\n# of pages: ${numPages}\nHas Read?: ${hasRead}`);
-       // submitBook(title,author,numPages,hasRead)
+        submitBook(title,author,numPages,hasRead)
     }
    
 }
 function submitBook(title,author,numPages,hasRead){
    
-    
+    console.log(`The book, ${title}, has been submitted yo`);
+    closeForm();
     
 }
 function cancelBook(){
     closeForm();
+    bookForm.classList.add('invisible');
+    bookForm.classList.remove('visible');
 }
 function closeForm(){
-   // const bookForm=document.getElementById('newBookForm');
-    //bookForm.style.display='none';
+
+    document.getElementById('entryForm').reset();
+
+    
 
 }
